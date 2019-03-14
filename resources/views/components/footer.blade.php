f8fe35a985434d2aedc7db669ec44ea45dc317f8
KkyJim12 <jirakarnjim1@gmail.com>
2019-03-14 12:00:11 +0700

    Footer on progress !!
    Footer on progress !!

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,30 @@
+<div class="mt-5" style="background-color:#343a40!important;color:white; height:300px;">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3 mt-3">
+        การชำระเงิน
+        <br>
+        <br>
+        การจัดส่ง
+      </div>
+      <div class="col-md-3 mt-3">
+        ลิงค์
+      </div>
+      <div class="col-md-3 mt-3">
+        ติดต่อ
+      </div>
+      <div class="col-md-3 mt-3">
+        เฟสบุ๊ค
+      </div>
+    </div>
+  </div>
+</div>
+<div style="background-color:black;color:white;">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        ©Copyright 2018, All Rights Reserved | Designed by Gachame
+      </div>
+    </div>
+  </div>
+</div>
