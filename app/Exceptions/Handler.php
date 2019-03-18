03c3614f24179e471cb719bc0f8e309421075827
KkyJim14 <jirakarnjim4@gmail.com>
2019-03-18 18:03:08 +0700

    Error 404
    Error 404

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,28 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+      if($this->isHttpException($exception))
+      {
+          switch ($exception->getStatusCode())
+              {
+              // not found
+              case 404:
+              return redirect('/');
+              break;
+
+              // internal error
+              case '500':
+              return redirect('/');
+              break;
+
+              default:
+                  return $this->renderHttpException($e);
+              break;
+          }
+      }
+      else {
         return parent::render($request, $exception);
+      }
+
     }
 }
