3ce1ef555de5a29c48cef75fbca41ebc2f695206
KkyJim14 <jirakarnjim4@gmail.com>
2019-03-18 21:41:07 +0700

    transfer fail
    transfer fail

--- app/Http/Controllers/OmiseController.php
+++ app/Http/Controllers/OmiseController.php
@@ -9,32 +9,37 @@ class OmiseController extends Controller
 {
     public function OmisePay(Request $request)  {
 
-      $validatedData = $request->validate([
-        'transfer_omise' => 'required',
-      ]);
+      if (session('user_log')) {
+        $validatedData = $request->validate([
+          'transfer_omise' => 'required',
+        ]);
 
-      if ($request->transfer_omise < 20) {
-        return redirect()->back()->with('omise-fail','กรุณาใส่จำนวนมากกว่า 20 บาท');
+        if ($request->transfer_omise < 20) {
+          return redirect()->back()->with('omise-fail','กรุณาใส่จำนวนมากกว่า 20 บาท');
+        }
+        else {
+          define('OMISE_PUBLIC_KEY', 'pkey_test_5cxodoewdmtrmj4j1g4');
+          define('OMISE_SECRET_KEY', 'skey_test_5cxodoewlzrw2k4dxlg');
+          define('OMISE_API_VERSION', '2017-11-02');
+
+          $charge = \OmiseCharge::create(array(
+          'card' => $request->omiseToken,
+          'amount' => (integer) $request->transfer_omise.'00',
+          'currency' => 'thb',
+          'description' => ''
+          ));
+
+          if ($charge['status'] == 'successful') {
+            $user = User::find(session('user_id'));
+            $user->user_money = $user->user_money+$request->transfer_omise;
+            $user->save();
+            $request->session()->put('user_money',$user->user_money);
+            return redirect()->bacK();
+          }
+        }
       }
       else {
-        define('OMISE_PUBLIC_KEY', 'pkey_test_5cxodoewdmtrmj4j1g4');
-        define('OMISE_SECRET_KEY', 'skey_test_5cxodoewlzrw2k4dxlg');
-        define('OMISE_API_VERSION', '2017-11-02');
-
-        $charge = \OmiseCharge::create(array(
-        'card' => $request->omiseToken,
-        'amount' => (integer) $request->transfer_omise.'00',
-        'currency' => 'thb',
-        'description' => ''
-        ));
-
-        if ($charge['status'] == 'successful') {
-          $user = User::find(session('user_id'));
-          $user->user_money = $user->user_money+$request->transfer_omise;
-          $user->save();
-          $request->session()->put('user_money',$user->user_money);
-          return redirect()->bacK();
-        }
+        return redirect()->route('member')->with('transfer-fail','กรุณาสมัครสมาชิกก่อน')
       }
     }
 }
--- app/Http/Controllers/TokenTransferController.php
+++ app/Http/Controllers/TokenTransferController.php
@@ -10,19 +10,51 @@ class TokenTransferController extends Controller
 {
     public function TokenTransferProcess(Request $request)  {
 
-      $validatedData = $request->validate([
-        'token_transfer' => 'required',
-      ]);
+      if(session('user_log')){
+        $validatedData = $request->validate([
+          'token_transfer' => 'required',
+        ]);
 
-      $token = Token::find($request->token_transfer);
-      if ($token) {
+        $token = Token::find($request->token_transfer);
+        if ($token) {
+          $user = User::find(session('user_id'));
+          if ($user->user_money < $token->token_pay) {
+            return redirect()->back()->with('transfer_fail','จำนวนเงินคุณไม่พอ');
+          }
+          else {
+            $user->user_money = $user->user_money-$token->token_pay;
+            $user->user_token = $user->user_token+$token->token_get;
+            $user->save();
+
+            $request->session()->put('user_money', $user->user_money);
+            $request->session()->put('user_token', $user->user_token);
+
+            return redirect()->back();
+          }
+        }
+        else {
+          return redirect('/');
+        }
+      }
+      else {
+        return redirect()->route('member')->with('transfer-fail','กรุณาสมัครสมาชิกก่อน');
+      }
+    }
+
+    public function TokenTransferManualProcess(Request $request)  {
+
+      if (session('user_log')) {
+        $validatedData = $request->validate([
+          'token_transfer' => 'required',
+        ]);
+
         $user = User::find(session('user_id'));
-        if ($user->user_money < $token->token_pay) {
+        if ($user->user_money < $request->token_transfer) {
           return redirect()->back()->with('transfer_fail','จำนวนเงินคุณไม่พอ');
         }
         else {
-          $user->user_money = $user->user_money-$token->token_pay;
-          $user->user_token = $user->user_token+$token->token_get;
+          $user->user_money = $user->user_money-$request->token_transfer;
+          $user->user_token = $user->user_token+$request->token_transfer;
           $user->save();
 
           $request->session()->put('user_money', $user->user_money);
@@ -32,29 +64,7 @@ class TokenTransferController extends Controller
         }
       }
       else {
-        return redirect('/');
-      }
-    }
-
-    public function TokenTransferManualProcess(Request $request)  {
-
-      $validatedData = $request->validate([
-        'token_transfer' => 'required',
-      ]);
-
-      $user = User::find(session('user_id'));
-      if ($user->user_money < $request->token_transfer) {
-        return redirect()->back()->with('transfer_fail','จำนวนเงินคุณไม่พอ');
-      }
-      else {
-        $user->user_money = $user->user_money-$request->token_transfer;
-        $user->user_token = $user->user_token+$request->token_transfer;
-        $user->save();
-
-        $request->session()->put('user_money', $user->user_money);
-        $request->session()->put('user_token', $user->user_token);
-
-        return redirect()->back();
+        return redirect()->route('member')->with('transfer-fail','กรุณาสมัครสมาชิกก่อน');
       }
     }
 }
